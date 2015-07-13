b178ec46b35f2ff328a26ed90db5323179d7b824
abdullahalbar <abdullah.albar@outlook.com>
2015-07-13 12:16:57 -0500

    second

--- SidebarMenu/pc.swift
+++ SidebarMenu/pc.swift
@@ -1,19 +1,19 @@
 //
-//  PhotoViewController.swift
+//  pc.swift
 //  SidebarMenu
 //
-//  Created by Simon Ng on 2/2/15.
-//  Copyright (c) 2015 AppCoda. All rights reserved.
+//  Created by abdullah albar on 7/13/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
 //
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class pcViewController: UIViewController {
     @IBOutlet weak var menuButton:UIBarButtonItem!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
             menuButton.action = "revealToggle:"
@@ -21,21 +21,21 @@ class PhotoViewController: UIViewController {
         }
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-       // Get the new view controller using segue.destinationViewController.
-       // Pass the selected object to the new view controller.
+      // Get the new view controller using segue.destinationViewController.
+      // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
